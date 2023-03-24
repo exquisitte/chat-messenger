698a3e6e50eb457e225a84571248d1e29588481a
howtly <timofymatesha2@gmail.com>
2023-03-24 12:53:56 +0200

    commit

---
+++ my-app/src/App.js
@@ -0,0 +1,9 @@
+import Register from "./pages/Register"
+import "./main.css"
+function App(){
+	return(
+		<Register/>
+	)
+}
+
+export default App;
---
+++ my-app/src/pages/Register.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+const Register = () => {
+  return (
+    <div>
+      <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Register</span>
+        <form>
+          <input type="text" placeholder="Display name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input style={{ display: "none" }}  type="file" />
+          <label htmlFor="file">
+            <span>Add an avatar</span>
+          </label>
+          <button>Sign up</button>
+        </form>
+        <p>
+          Maybe you have an account? <a href="./pages/Login">Login</a>
+        </p>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Register
